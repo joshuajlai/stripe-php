7423fe8ce5d31a9c3d956317bce03833f0ac0ebf
Olivier Bellone <ob@stripe.com>
2019-10-18 12:12:23 -0700

    Add AbstractService and a couple of concrete services

--- init.php
+++ init.php
@@ -152,3 +152,8 @@ require(dirname(__FILE__) . '/lib/OAuthErrorObject.php');
 // Webhooks
 require(dirname(__FILE__) . '/lib/Webhook.php');
 require(dirname(__FILE__) . '/lib/WebhookSignature.php');
+
+// Services
+require(dirname(__FILE__) . '/lib/Service/AbstractService.php');
+require(dirname(__FILE__) . '/lib/Service/CouponService.php');
+require(dirname(__FILE__) . '/lib/Service/PaymentIntentService.php');
