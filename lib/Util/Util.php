93b07119565674d58749ab958e9b3442c96a587c
Joshua J Lai <jlai@slack-corp.com>
2020-01-16 12:31:55 -0800

    Try to leverage `get_declared_classes` to fetch all available Stripe models so that `convertToStripeObject` does not need to maintain a mapping between object names in the openapi spec and Stripe classes.

--- lib/Util/Util.php
+++ lib/Util/Util.php
@@ -8,6 +8,7 @@ abstract class Util
 {
     private static $isMbstringAvailable = null;
     private static $isHashEqualsAvailable = null;
+    private static $type_map = [];
 
     /**
      * Whether the provided array (or other) is a list rather than a dictionary.
@@ -31,6 +32,7 @@ abstract class Util
         return true;
     }
 
+
     /**
      * Converts a response from the Stripe API to the corresponding PHP object.
      *
@@ -40,89 +42,8 @@ abstract class Util
      */
     public static function convertToStripeObject($resp, $opts)
     {
-        $types = [
-            // data structures
-            \Stripe\Collection::OBJECT_NAME => \Stripe\Collection::class,
+        $types = self::getTypes();
 
-            // business objects
-            \Stripe\Account::OBJECT_NAME => \Stripe\Account::class,
-            \Stripe\AccountLink::OBJECT_NAME => \Stripe\AccountLink::class,
-            \Stripe\AlipayAccount::OBJECT_NAME => \Stripe\AlipayAccount::class,
-            \Stripe\ApplePayDomain::OBJECT_NAME => \Stripe\ApplePayDomain::class,
-            \Stripe\ApplicationFee::OBJECT_NAME => \Stripe\ApplicationFee::class,
-            \Stripe\ApplicationFeeRefund::OBJECT_NAME => \Stripe\ApplicationFeeRefund::class,
-            \Stripe\Balance::OBJECT_NAME => \Stripe\Balance::class,
-            \Stripe\BalanceTransaction::OBJECT_NAME => \Stripe\BalanceTransaction::class,
-            \Stripe\BankAccount::OBJECT_NAME => \Stripe\BankAccount::class,
-            \Stripe\BitcoinReceiver::OBJECT_NAME => \Stripe\BitcoinReceiver::class,
-            \Stripe\BitcoinTransaction::OBJECT_NAME => \Stripe\BitcoinTransaction::class,
-            \Stripe\Capability::OBJECT_NAME => \Stripe\Capability::class,
-            \Stripe\Card::OBJECT_NAME => \Stripe\Card::class,
-            \Stripe\Charge::OBJECT_NAME => \Stripe\Charge::class,
-            \Stripe\Checkout\Session::OBJECT_NAME => \Stripe\Checkout\Session::class,
-            \Stripe\CountrySpec::OBJECT_NAME => \Stripe\CountrySpec::class,
-            \Stripe\Coupon::OBJECT_NAME => \Stripe\Coupon::class,
-            \Stripe\CreditNote::OBJECT_NAME => \Stripe\CreditNote::class,
-            \Stripe\Customer::OBJECT_NAME => \Stripe\Customer::class,
-            \Stripe\CustomerBalanceTransaction::OBJECT_NAME => \Stripe\CustomerBalanceTransaction::class,
-            \Stripe\Discount::OBJECT_NAME => \Stripe\Discount::class,
-            \Stripe\Dispute::OBJECT_NAME => \Stripe\Dispute::class,
-            \Stripe\EphemeralKey::OBJECT_NAME => \Stripe\EphemeralKey::class,
-            \Stripe\Event::OBJECT_NAME => \Stripe\Event::class,
-            \Stripe\ExchangeRate::OBJECT_NAME => \Stripe\ExchangeRate::class,
-            \Stripe\File::OBJECT_NAME => \Stripe\File::class,
-            \Stripe\File::OBJECT_NAME_ALT => \Stripe\File::class,
-            \Stripe\FileLink::OBJECT_NAME => \Stripe\FileLink::class,
-            \Stripe\Invoice::OBJECT_NAME => \Stripe\Invoice::class,
-            \Stripe\InvoiceItem::OBJECT_NAME => \Stripe\InvoiceItem::class,
-            \Stripe\InvoiceLineItem::OBJECT_NAME => \Stripe\InvoiceLineItem::class,
-            \Stripe\Issuing\Authorization::OBJECT_NAME => \Stripe\Issuing\Authorization::class,
-            \Stripe\Issuing\Card::OBJECT_NAME => \Stripe\Issuing\Card::class,
-            \Stripe\Issuing\CardDetails::OBJECT_NAME => \Stripe\Issuing\CardDetails::class,
-            \Stripe\Issuing\Cardholder::OBJECT_NAME => \Stripe\Issuing\Cardholder::class,
-            \Stripe\Issuing\Dispute::OBJECT_NAME => \Stripe\Issuing\Dispute::class,
-            \Stripe\Issuing\Transaction::OBJECT_NAME => \Stripe\Issuing\Transaction::class,
-            \Stripe\LoginLink::OBJECT_NAME => \Stripe\LoginLink::class,
-            \Stripe\Order::OBJECT_NAME => \Stripe\Order::class,
-            \Stripe\OrderItem::OBJECT_NAME => \Stripe\OrderItem::class,
-            \Stripe\OrderReturn::OBJECT_NAME => \Stripe\OrderReturn::class,
-            \Stripe\PaymentIntent::OBJECT_NAME => \Stripe\PaymentIntent::class,
-            \Stripe\PaymentMethod::OBJECT_NAME => \Stripe\PaymentMethod::class,
-            \Stripe\Payout::OBJECT_NAME => \Stripe\Payout::class,
-            \Stripe\Person::OBJECT_NAME => \Stripe\Person::class,
-            \Stripe\Plan::OBJECT_NAME => \Stripe\Plan::class,
-            \Stripe\Product::OBJECT_NAME => \Stripe\Product::class,
-            \Stripe\Radar\EarlyFraudWarning::OBJECT_NAME => \Stripe\Radar\EarlyFraudWarning::class,
-            \Stripe\Radar\ValueList::OBJECT_NAME => \Stripe\Radar\ValueList::class,
-            \Stripe\Radar\ValueListItem::OBJECT_NAME => \Stripe\Radar\ValueListItem::class,
-            \Stripe\Recipient::OBJECT_NAME => \Stripe\Recipient::class,
-            \Stripe\RecipientTransfer::OBJECT_NAME => \Stripe\RecipientTransfer::class,
-            \Stripe\Refund::OBJECT_NAME => \Stripe\Refund::class,
-            \Stripe\Reporting\ReportRun::OBJECT_NAME => \Stripe\Reporting\ReportRun::class,
-            \Stripe\Reporting\ReportType::OBJECT_NAME => \Stripe\Reporting\ReportType::class,
-            \Stripe\Review::OBJECT_NAME => \Stripe\Review::class,
-            \Stripe\SetupIntent::OBJECT_NAME => \Stripe\SetupIntent::class,
-            \Stripe\Sigma\ScheduledQueryRun::OBJECT_NAME => \Stripe\Sigma\ScheduledQueryRun::class,
-            \Stripe\SKU::OBJECT_NAME => \Stripe\SKU::class,
-            \Stripe\Source::OBJECT_NAME => \Stripe\Source::class,
-            \Stripe\SourceTransaction::OBJECT_NAME => \Stripe\SourceTransaction::class,
-            \Stripe\Subscription::OBJECT_NAME => \Stripe\Subscription::class,
-            \Stripe\SubscriptionItem::OBJECT_NAME => \Stripe\SubscriptionItem::class,
-            \Stripe\SubscriptionSchedule::OBJECT_NAME => \Stripe\SubscriptionSchedule::class,
-            \Stripe\TaxId::OBJECT_NAME => \Stripe\TaxId::class,
-            \Stripe\TaxRate::OBJECT_NAME => \Stripe\TaxRate::class,
-            \Stripe\ThreeDSecure::OBJECT_NAME => \Stripe\ThreeDSecure::class,
-            \Stripe\Terminal\ConnectionToken::OBJECT_NAME => \Stripe\Terminal\ConnectionToken::class,
-            \Stripe\Terminal\Location::OBJECT_NAME => \Stripe\Terminal\Location::class,
-            \Stripe\Terminal\Reader::OBJECT_NAME => \Stripe\Terminal\Reader::class,
-            \Stripe\Token::OBJECT_NAME => \Stripe\Token::class,
-            \Stripe\Topup::OBJECT_NAME => \Stripe\Topup::class,
-            \Stripe\Transfer::OBJECT_NAME => \Stripe\Transfer::class,
-            \Stripe\TransferReversal::OBJECT_NAME => \Stripe\TransferReversal::class,
-            \Stripe\UsageRecord::OBJECT_NAME => \Stripe\UsageRecord::class,
-            \Stripe\UsageRecordSummary::OBJECT_NAME => \Stripe\UsageRecordSummary::class,
-            \Stripe\WebhookEndpoint::OBJECT_NAME => \Stripe\WebhookEndpoint::class,
-        ];
         if (self::isList($resp)) {
             $mapped = [];
             foreach ($resp as $i) {
@@ -131,14 +52,36 @@ abstract class Util
             return $mapped;
         } elseif (is_array($resp)) {
             if (isset($resp['object']) && is_string($resp['object']) && isset($types[$resp['object']])) {
-                $class = $types[$resp['object']];
+                $class_reflection = $types[$resp['object']];
             } else {
-                $class = \Stripe\StripeObject::class;
+                $class_reflection = new \ReflectionClass('\Stripe\StripeObject');
             }
-            return $class::constructFrom($resp, $opts);
+            $method = $class_reflection->getMethod('constructFrom');
+            return $method->invoke(null, $resp, $opts);
         } else {
             return $resp;
         }
+    }
+
+    public static function getTypes(): array {
+        if (!empty(self::$type_map)) {
+            return self::$type_map;
+        }
+        $types = [];
+        foreach (\get_declared_classes() as $classname) {
+            if (0 !== \strpos($classname, 'Stripe')) {
+                continue;
+            }
+
+            $class_reflection = new \ReflectionClass($classname);
+            $constants = $class_reflection->getConstants();
+            if (!\array_key_exists('OBJECT_NAME', $constants)) {
+                continue;
+            }
+            $types[(string) $constants['OBJECT_NAME']] = $class_reflection;
+        }
+        self::$type_map = $types;
+        return self::$type_map;
     }
 
     /**
